9bf9e7fd468b44ae92a55492a6e417d90f7e0a39
Ziyi Liu <78235583+Ziyi-star@users.noreply.github.com>
2026-08-22 14:05:22 +0200

    Add initial Docker configuration files

--- app/model.py
+++ app/model.py
@@ -19,4 +19,10 @@ classes = [
 
 
 #preprocessing helper function: segment_acceleration_data_overlapping_numpy
+def predict_pipeline(text):
+    text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', " ", text)
+    text = re.sub(r"[[]]", " ", text)
+    text = text.lower()
+    pred = model.predict([text])
+    return classes[pred[0]]
     
